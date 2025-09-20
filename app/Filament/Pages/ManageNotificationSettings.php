15cb98f3670892b6e7063a399f4851eefd4a3385
Faruk Nasir <frknasir@yahoo.com>
2025-09-20 20:20:28 +0100

    Refactor notification settings form to use camelCase for field names

--- app/Filament/Pages/ManageNotificationSettings.php
+++ app/Filament/Pages/ManageNotificationSettings.php
@@ -33,28 +33,28 @@ class ManageNotificationSettings extends SettingsPage
                 Section::make('Deal Created Notifications')
                     ->description('Configure who gets notified when new deals are created')
                     ->schema([
-                        Toggle::make('deal_created_enabled')
+                        Toggle::make('dealCreatedEnabled')
                             ->label('Enable Deal Created Notifications')
                             ->helperText('Turn on/off all deal created notifications'),
 
                         Grid::make(2)
                             ->schema([
-                                Toggle::make('deal_created_email_enabled')
+                                Toggle::make('dealCreatedEmailEnabled')
                                     ->label('Email Notifications')
                                     ->helperText('Send email notifications'),
 
-                                Toggle::make('deal_created_database_enabled')
+                                Toggle::make('dealCreatedDatabaseEnabled')
                                     ->label('In-App Notifications')
                                     ->helperText('Show in-app notifications'),
                             ]),
 
-                        Select::make('deal_created_roles')
+                        Select::make('dealCreatedRoles')
                             ->label('Notify Users with Roles')
                             ->multiple()
                             ->options(Role::pluck('name', 'name')->toArray())
                             ->helperText('Users with these roles will receive notifications'),
 
-                        Select::make('deal_created_users')
+                        Select::make('dealCreatedUsers')
                             ->label('Additional Users to Notify')
                             ->multiple()
                             ->searchable()
@@ -75,24 +75,24 @@ class ManageNotificationSettings extends SettingsPage
                 Section::make('Deal Stage Changed Notifications')
                     ->description('Configure who gets notified when deals move between stages')
                     ->schema([
-                        Toggle::make('deal_stage_changed_enabled')
+                        Toggle::make('dealStageChangedEnabled')
                             ->label('Enable Stage Change Notifications'),
 
                         Grid::make(2)
                             ->schema([
-                                Toggle::make('deal_stage_changed_email_enabled')
+                                Toggle::make('dealStageChangedEmailEnabled')
                                     ->label('Email Notifications'),
 
-                                Toggle::make('deal_stage_changed_database_enabled')
+                                Toggle::make('dealStageChangedDatabaseEnabled')
                                     ->label('In-App Notifications'),
                             ]),
 
-                        Select::make('deal_stage_changed_roles')
+                        Select::make('dealStageChangedRoles')
                             ->label('Notify Users with Roles')
                             ->multiple()
                             ->options(Role::pluck('name', 'name')->toArray()),
 
-                        Select::make('deal_stage_changed_users')
+                        Select::make('dealStageChangedUsers')
                             ->label('Additional Users to Notify')
                             ->multiple()
                             ->searchable()
@@ -113,24 +113,24 @@ class ManageNotificationSettings extends SettingsPage
                 Section::make('Deal Won Notifications')
                     ->description('Configure who gets notified when deals are marked as won')
                     ->schema([
-                        Toggle::make('deal_won_enabled')
+                        Toggle::make('dealWonEnabled')
                             ->label('Enable Deal Won Notifications'),
 
                         Grid::make(2)
                             ->schema([
-                                Toggle::make('deal_won_email_enabled')
+                                Toggle::make('dealWonEmailEnabled')
                                     ->label('Email Notifications'),
 
-                                Toggle::make('deal_won_database_enabled')
+                                Toggle::make('dealWonDatabaseEnabled')
                                     ->label('In-App Notifications'),
                             ]),
 
-                        Select::make('deal_won_roles')
+                        Select::make('dealWonRoles')
                             ->label('Notify Users with Roles')
                             ->multiple()
                             ->options(Role::pluck('name', 'name')->toArray()),
 
-                        Select::make('deal_won_users')
+                        Select::make('dealWonUsers')
                             ->label('Additional Users to Notify')
                             ->multiple()
                             ->searchable()
@@ -150,24 +150,24 @@ class ManageNotificationSettings extends SettingsPage
                 Section::make('Deal Lost Notifications')
                     ->description('Configure who gets notified when deals are marked as lost')
                     ->schema([
-                        Toggle::make('deal_lost_enabled')
+                        Toggle::make('dealLostEnabled')
                             ->label('Enable Deal Lost Notifications'),
 
                         Grid::make(2)
                             ->schema([
-                                Toggle::make('deal_lost_email_enabled')
+                                Toggle::make('dealLostEmailEnabled')
                                     ->label('Email Notifications'),
 
-                                Toggle::make('deal_lost_database_enabled')
+                                Toggle::make('dealLostDatabaseEnabled')
                                     ->label('In-App Notifications'),
                             ]),
 
-                        Select::make('deal_lost_roles')
+                        Select::make('dealLostRoles')
                             ->label('Notify Users with Roles')
                             ->multiple()
                             ->options(Role::pluck('name', 'name')->toArray()),
 
-                        Select::make('deal_lost_users')
+                        Select::make('dealLostUsers')
                             ->label('Additional Users to Notify')
                             ->multiple()
                             ->searchable()
@@ -187,25 +187,25 @@ class ManageNotificationSettings extends SettingsPage
                 Section::make('Deal Assignment Notifications')
                     ->description('Configure who gets notified when deals are reassigned')
                     ->schema([
-                        Toggle::make('deal_assigned_enabled')
+                        Toggle::make('dealAssignedEnabled')
                             ->label('Enable Deal Assignment Notifications'),
 
                         Grid::make(2)
                             ->schema([
-                                Toggle::make('deal_assigned_email_enabled')
+                                Toggle::make('dealAssignedEmailEnabled')
                                     ->label('Email Notifications'),
 
-                                Toggle::make('deal_assigned_database_enabled')
+                                Toggle::make('dealAssignedDatabaseEnabled')
                                     ->label('In-App Notifications'),
                             ]),
 
-                        Select::make('deal_assigned_roles')
+                        Select::make('dealAssignedRoles')
                             ->label('Notify Users with Roles')
                             ->multiple()
                             ->options(Role::pluck('name', 'name')->toArray())
                             ->helperText('Deal owner and previous owner are always notified'),
 
-                        Select::make('deal_assigned_users')
+                        Select::make('dealAssignedUsers')
                             ->label('Additional Users to Notify')
                             ->multiple()
                             ->searchable()
